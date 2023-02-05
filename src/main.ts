ca1ece7cbccdeb83298c1443c0568a3c1a239a72
yunacho96 <whdbsk9520@naver.com>
2023-02-05 12:51:54 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModules } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModules);
   await app.listen(3000);
 }
 bootstrap();
